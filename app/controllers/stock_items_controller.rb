c9cc9ade8e0bf6a2bff5bffbe4dd9470fd1b30e5
João <joaopfernandesc@gmail.com>
2020-08-31 20:16:08 -0300

    Added controllers backbone

---
+++ app/controllers/stock_items_controller.rb
@@ -0,0 +1,17 @@
+class StockItemsController < ApplicationController
+  def create
+    
+  end
+  def update
+    
+  end
+  def index
+    
+  end
+  def show
+    
+  end
+  def destroy
+    
+  end
+end
